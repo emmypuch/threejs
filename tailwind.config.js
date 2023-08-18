60936ff2e28444d41defa7c51d6a9d5a3afd6a42
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-08-18 02:24:10 +0100

    building a canvas customizer page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px", // Make sure this definition is present
+    },
+  },
+  plugins: [],
+};
